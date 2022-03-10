9c5932e6f90f269d874eb2d1bee492e9bb655891
iea32 <iea32@mail.ru>
2022-03-10 14:12:35 +0500

    add app portal

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -1,4 +1,4 @@
-"""Portal URL Configuration
+"""NewsPortal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
