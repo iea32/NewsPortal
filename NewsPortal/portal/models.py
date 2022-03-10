d86719ec9cb12bf0f20787436196aa5e40e377e4
iea32 <iea32@mail.ru>
2022-03-10 15:23:43 +0500

    edit models.py

--- NewsPortal/portal/models.py
+++ NewsPortal/portal/models.py
@@ -1,18 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
 
 class Author(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    rating = models.IntegerField(default = 0)
+
+    def update_rating(self, new_rating):
+        self.rating = new_rating
+        self.save()
+
+    def __str__(self):
+        return f'{self.user}'
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length = 255, unique = True)
+
+    def __str__(self):
+        return f'{self.name.title()}'
+
 
 class Post(models.Model):
-    pass
+    article = 'a'
+    news = 'n'
+
+    POST_TYPE = [
+        (article, "Статья"),
+        (news, "Новость")
+    ]
+
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    post_type = models.CharField(max_length = 1, choices = POST_TYPE, default = article)
+    created = models.DateTimeField(auto_now_add = True)
+    cats = models.ManyToManyField(Category, through = 'PostCategory')
+    title = models.CharField(max_length = 255)
+    text = models.TextField()
+    rating = models.IntegerField(default = 0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+    def __str__(self):
+        return f'{self.title}: {self.text[:20]}'
+
+    def get_absolute_url(self):  # добавим абсолютный путь, чтобы после создания нас перебрасывало на страницу с товаром
+        return f'/posts/{self.id}'
 
 class PostCategory(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+    rating = models.IntegerField(default = 0)
 
+    def like(self):
+        self.rating += 1
+        self.save()
 
+    def dislike(self):
+        self.rating -= 1
+        self.save()
